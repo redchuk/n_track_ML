318c5adfb184522c6aade266613abb47c598da61
Taras Redchuk <taras.redchuk@gmail.com>
2021-07-05 15:04:24 +0300

    add more features, e.g. persistence and total displacement

--- scripts/n_track_DNN.py
+++ scripts/n_track_DNN.py
@@ -1,19 +1,31 @@
 import pandas as pd
+import numpy as np
 
-
-# reading the data
+''' read the data '''
 data = pd.read_csv('scripts/data_chromatin_live.csv')
 
-# add features
+''' add features '''
 data_agg = data.groupby(['file', 'particle']).agg(guide=('guide', 'first'),
                                                   time=('time', 'first'),
                                                   serum_conc_percent=('serum_conc_percent', 'first'),
+
                                                   mean_diff_xy_micron=('diff_xy_micron', 'mean'),
-                                                  max_diff_xy_micron=('diff_xy_micron', 'max'),
+                                                  # average displacement
+                                                  max_diff_xy_micron=('diff_xy_micron', 'max'),  # maximal displacement
                                                   sum_diff_xy_micron=('diff_xy_micron', 'sum'),
+                                                  # total trajectory length
+
+                                                  sum_diff_x_micron=('diff_x_micron', 'sum'),
+                                                  sum_diff_y_micron=('diff_y_micron', 'sum'),
+
                                                   )
 
+data_agg['total_displacement'] = np.sqrt((data_agg['sum_diff_x_micron'])**2 + (data_agg['sum_diff_y_micron'])**2)
+# distance from first to last coordinate
+data_agg['persistence'] = data_agg['total_displacement']/data_agg['sum_diff_xy_micron']
+# shows how directional the movement is
 
 
-# stratified cross-val K fold
 
+
+''' stratified cross-val K fold '''
