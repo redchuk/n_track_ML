65cb3281cb92169d90a2d7451dd0f644cf2871fd
Taras Redchuk <taras.redchuk@gmail.com>
2021-09-16 15:48:37 +0300

    add function to prepare data for Dabest plotting

--- scripts/plotting.py
+++ scripts/plotting.py
@@ -1,1 +1,16 @@
+import pandas as pd
+import dabest
+
 data = pd.read_csv('scripts/data_sterile_449e453.csv')
+
+def gen_set(data, parameter, chr_name, guides):
+    '''
+
+    :param data: df
+    :param parameter: feature to plot, str
+    :param chr_name: chr name, for labeling
+    :param guides: guides to aggregate
+    :return: df for Dabest plotting
+    '''
+
+    data = data[data["t_guide"].str.contains('1398') | data["t_guide"].str.contains('1514')]
