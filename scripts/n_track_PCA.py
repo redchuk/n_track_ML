b49b9ea8bc510bde8fef924b966b5f53f76df70b
Taras Redchuk <taras.redchuk@gmail.com>
2021-10-28 15:27:31 +0300

    create dataset (scripts/b212a935_Chr1_data_sterile.csv) and script (n_track_PCA.py) to try PCA and similar

---
+++ scripts/n_track_PCA.py
@@ -0,0 +1,12 @@
+'''
+This is to try unsupervised learning (PCA, UMAP etc) for feature analysis, dimensionality
+reduction, visualization, importance estimation.
+'''
+
+import pandas as pd
+
+''' 
+read the data 
+'''
+
+data = pd.read_csv('scripts/b212a935_Chr1_data_sterile.csv')
