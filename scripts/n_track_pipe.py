733d45d3be96adfe0cabc17d9393e3663d16ecf4
Taras Redchuk <taras.redchuk@gmail.com>
2021-12-22 17:22:54 +0200

    PCA+GBC pipeline

--- scripts/n_track_pipe.py
+++ scripts/n_track_pipe.py
@@ -17,10 +17,14 @@ Options for preprocessing:
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
+from sklearn.model_selection import GroupKFold, GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.pipeline import Pipeline
+from sklearn.ensemble import GradientBoostingClassifier
 import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
 
 data = pd.read_csv('scripts/b212a935_Chr1_data_sterile.csv')
 
@@ -47,10 +51,37 @@ PCA_transformer = Pipeline(
 
 c_transformer = ColumnTransformer(
     [('f_to_retain', SimpleImputer(missing_values=np.nan, strategy='mean'), fset_all),
-     # (hopefully) does nothing
+     # (hopefully) does nothing, placed here to keep original features in pipeline output
      ('PCA_scaled', PCA_transformer, fset_no_masks)]
 )
 
 X_t = c_transformer.fit_transform(X)
-# seems it works, GET FEATURE NAMES!!!
-# add classifier
+# seems it works, is there an easy way to get feature names?
+
+"""
+PCA + GBC pipeline
+"""
+
+boosted_forest = GradientBoostingClassifier(n_estimators=1000)
+gkf = GroupKFold(n_splits=4)
+
+gbc_pipeline = Pipeline(
+    steps=[('preproc_PCA', c_transformer),
+           ('GBC', boosted_forest)]
+)
+
+b_param_grid = {'GBC__learning_rate': [0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10],
+                'GBC__max_depth': [1, 2, 3, 4, 5, 10, 20, 30, 40]}
+
+b_grid_search = GridSearchCV(gbc_pipeline, b_param_grid, cv=gkf,
+                             refit=False)
+b_grid_search.fit(X, y, groups=data['file'])
+b_grid_forest_results = pd.DataFrame(b_grid_search.cv_results_)
+
+b_pvt = pd.pivot_table(b_grid_forest_results,
+                       values='mean_test_score',
+                       index='param_GBC__learning_rate',
+                       columns='param_GBC__max_depth')
+sns.heatmap(b_pvt, annot=True)
+plt.show()
+plt.close()
