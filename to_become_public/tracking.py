6f654341fc7f973faab1683a763875543df4f273
Taras Redchuk <taras.redchuk@gmail.com>
2022-09-29 16:07:26 +0300

    code cleaning

--- to_become_public/tracking.py
+++ to_become_public/tracking.py
@@ -59,23 +59,22 @@ def regis(chrom, lamin, fast=False):
     return chrom, ch_mask, ch_edges
 
 
-def find_dots_TL(stack, minmass=100000, diameter=11, search_range=20, memory=3, threshold=20):
+def find_dots(stack, minmass=100000, diameter=11, search_range=20, memory=3, threshold=20):
     tp.quiet()
     f = tp.batch(stack, diameter=diameter, minmass=minmass)
-    #todo: can it be just 'parameter' instead of 'parameter = parameter'?
+    # todo: can it be just 'parameter' instead of 'parameter = parameter'?
     t = tp.link(f, search_range, memory=memory)
     t1 = tp.filter_stubs(t, threshold)
     return t1
 
 
-def min_dist_TL(df, edges_stack):
-
+def min_dist(df, edges_stack):
     coord_x = np.asarray([i for i in range(edges_stack[0].shape[1])])
     coord_y = np.asarray([i for i in range(edges_stack[0].shape[0])])
 
-    xarr = np.zeros((edges_stack[0].shape))
+    xarr = np.zeros(edges_stack[0].shape)
     xarr[:, :] = coord_x
-    yarr = np.zeros((edges_stack[0].shape)).T
+    yarr = np.zeros(edges_stack[0].shape).T
     yarr[:, :] = coord_y
 
     arr = np.sqrt((xarr - df['x']) ** 2 + (yarr.T - df['y']) ** 2)
@@ -83,8 +82,7 @@ def min_dist_TL(df, edges_stack):
     return df
 
 
-def analyse_data(dots_df, nucl_masks, nucl_edges, pxsize, serum):
-
+def analyse_data(dots_df, nucl_masks, nucl_edges, pxsize, file):
     dots_df['y_micron'] = dots_df['y'] * pxsize
     dots_df['x_micron'] = dots_df['x'] * pxsize
 
@@ -94,85 +92,87 @@ def analyse_data(dots_df, nucl_masks, nucl_edges, pxsize, serum):
     dots_df['perimeter_au'] = perimeters[dots_df['frame']]
     dots_df['perimeter_au_norm'] = dots_df['perimeter_au'] * pxsize ** 2
 
-    data_dist = dots_df.apply(min_dist_TL, axis=1, edges_stack=nucl_edges)
+    data_dist = dots_df.apply(min_dist, axis=1, edges_stack=nucl_edges)
     data_dist['min_dist_micron'] = data_dist['min_dist_pxs'] * pxsize
+
     data_dist['script_version_git'] = get_git_hash()
-    data_dist['serum'] = serum
+    data_dist['serum'] = int(file.split('_')[1])
+    data_dist['date'] = file.split('_')[0]
+    data_dist['file'] = file[:-4]
+
     return data_dist
 
 
-"""def aggregate():
+def add_xy_deltas(df):
+    c_list = ['file',
+              'script_version_git',
+              'date',
+              'serum',
+              'particle',
+              'frame',
+              'y', 'x',
+              'y_micron', 'diff_y_micron',
+              'x_micron', 'diff_x_micron',
+              'diff_xy_micron',
+              'area_micron',
+              'perimeter_au',
+              'perimeter_au_norm',
+              'min_dist_pxs',
+              'min_dist_micron',
+              ]
 
-    aggregated = pd.DataFrame(columns=["file",
-                                       "script_version_git",
-                                       "date",
-                                       "guide",
-                                       "time",
-                                       "serum_conc_percent",
-                                       "particle",
-                                       "frame",
-                                       "y", "x",
-                                       "y_micron", "diff_y_micron",
-                                       "x_micron", "diff_x_micron",
-                                       "diff_xy_micron",
-                                       "diff_xy_micron**2",
-                                       "area_micron",
-                                       "perimeter_au",
-                                       "perimeter_au_norm",
-                                       "min_dist_pxs",
-                                       "min_dist_micron",
-                                       "sqrt(area/pi)",
-                                       "min_dist/(sqrt(area/pi))",
-                                       "comment", "comment_long"])
+    data_a = pd.DataFrame(columns=c_list)
+    dots = df['particle'].unique()
 
-    for i in glob.glob(o_path + "*"):
-        filename = str(i).split('\\')[-1]
-        data = pd.read_csv(i)
-        data["date"] = filename[4:12]
-        data["file"] = filename[4:-4]
+    for i in dots:
+        data_p = df[df["particle"] == i]
+        data_p["diff_y_micron"] = data_p["y_micron"].diff()
+        data_p["diff_x_micron"] = data_p["x_micron"].diff()
+        data_p["diff_xy_micron"] = ((data_p["diff_x_micron"]) ** 2 + (data_p["diff_y_micron"]) ** 2) ** .5
 
-        dots = data["particle"].unique()
-        for i in dots:
-            data_p = data[data["particle"] == i]
-            data_p["diff_y_micron"] = data_p["y_micron"].diff()
-            data_p["diff_x_micron"] = data_p["x_micron"].diff()
-            data_p["diff_xy_micron**2"] = ((data_p["diff_x_micron"]) ** 2 + (data_p["diff_y_micron"]) ** 2)
-            data_p["diff_xy_micron"] = data_p["diff_xy_micron**2"] ** .5
-            data_p["sqrt(area/pi)"] = (data_p["area_micron"] / math.pi) ** .5
-            data_p["min_dist/(sqrt(area/pi))"] = data_p["min_dist_micron"] / data_p["sqrt(area/pi)"]
-            aggregated = aggregated.append(data_p, ignore_index=True, sort=False)
-        print(filename + ' is ok')
-    aggregated.set_index(["file", "particle"], inplace=True)
+        data_a = data_a.append(data_p, ignore_index=True, sort=False)
 
-    aggregated.iloc[:, 0:26].to_csv(o_path[:-1] +
-                                    "_aggregated/aggregated_n_track.csv")
-"""
+    data_a = data_a.iloc[:, 0:len(c_list)]
+
+    return data_a
+
 
 # todo: test below to be removed, or make bash script from it? main method?
-
-
 inp_path = 'to_become_public/example_images/*ome.tif'
 out_path = 'to_become_public/tracking_output/'
 
+columns_raw = ['file',
+               'script_version_git',
+               'date',
+               'serum',
+               'particle',
+               'frame',
+               'y_micron',
+               'x_micron',
+               'diff_xy_micron',
+               'area_micron',
+               'perimeter_au_norm',
+               'min_dist_micron',
+               ]
 
-tstlst = glob.glob(inp_path, recursive=True)
-print(tstlst)
-filename = str(tstlst[1]).split('\\')[-1]
-print(filename)
+data_raw = pd.DataFrame(columns=columns_raw)
 
-(tiff, meta) = ome.read_ometiff(tstlst[1])
-xy_pxsize = float((meta.split(' ')[57]).split("\"")[1])
-# todo: parse xml as Harri explained, https://www.geeksforgeeks.org/xml-parsing-python/
-label_serum = int(filename.split('_')[1])
+for file in glob.glob(inp_path, recursive=True):
+    filename = str(file).split('\\')[-1]
 
-chrom = rescale_intensity(tiff[:,0,0,:,:])
-lamin = rescale_intensity(tiff[:,0,1,:,:])
-#plt.imshow(chrom[0,:,:])
-#plt.imshow(lamin[0,:,:])
+    (tiff, meta) = ome.read_ometiff(file)
+    xy_pxsize = float((meta.split(' ')[57]).split("\"")[1])
+    # todo: parse xml as Harri explained, https://www.geeksforgeeks.org/xml-parsing-python/
+    label_serum = int(filename.split('_')[1])
 
-chrom_r, masks, edges = regis(chrom, lamin, fast = False)
-data = find_dots_TL(chrom_r)
-#tp.annotate(data.iloc[:4], chrom_r[0,:,:])
+    chrom = rescale_intensity(tiff[:, 0, 0, :, :])
+    lamin = rescale_intensity(tiff[:, 0, 1, :, :])
 
-add_data = analyse_data(data, masks, edges, xy_pxsize, label_serum)
+    chrom_r, masks, edges = regis(chrom, lamin, fast=False)
+    data = find_dots(chrom_r)
 
+    add_data = analyse_data(data, masks, edges, xy_pxsize, filename)
+    add_data = add_xy_deltas(add_data)
+    data_raw = data_raw.append(add_data, ignore_index=True, sort=False)
+
+data_raw.iloc[:, 0:len(columns_raw)].to_csv(out_path + 'tracking_raw.csv')
