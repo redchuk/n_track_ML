24cf43418717f59793407222590ba263d07c544a
Taras Redchuk <taras.redchuk@gmail.com>
2022-09-29 11:17:37 +0300

    in analyse_data() extract serum concentration (label) from file name to omit df manual editing. inp_path fixed

--- to_become_public/tracking.py
+++ to_become_public/tracking.py
@@ -13,9 +13,6 @@ from pystackreg import StackReg
 import pandas as pd
 import subprocess
 
-inp_path = '**/*.tif'  # todo: fix path
-out_path = 'to_become_public/tracking_output/'
-
 
 def get_git_hash():
     return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
@@ -86,7 +83,7 @@ def min_dist_TL(df, edges_stack):
     return df
 
 
-def analyse_data(dots_df, nucl_masks, nucl_edges, pxsize):
+def analyse_data(dots_df, nucl_masks, nucl_edges, pxsize, serum):
 
     dots_df['y_micron'] = dots_df['y'] * pxsize
     dots_df['x_micron'] = dots_df['x'] * pxsize
@@ -100,10 +97,64 @@ def analyse_data(dots_df, nucl_masks, nucl_edges, pxsize):
     data_dist = dots_df.apply(min_dist_TL, axis=1, edges_stack=nucl_edges)
     data_dist['min_dist_micron'] = data_dist['min_dist_pxs'] * pxsize
     data_dist['script_version_git'] = get_git_hash()
-    data_dist['serum_conc_percent'] = ''
+    data_dist['serum'] = serum
     return data_dist
 
+
+"""def aggregate():
+
+    aggregated = pd.DataFrame(columns=["file",
+                                       "script_version_git",
+                                       "date",
+                                       "guide",
+                                       "time",
+                                       "serum_conc_percent",
+                                       "particle",
+                                       "frame",
+                                       "y", "x",
+                                       "y_micron", "diff_y_micron",
+                                       "x_micron", "diff_x_micron",
+                                       "diff_xy_micron",
+                                       "diff_xy_micron**2",
+                                       "area_micron",
+                                       "perimeter_au",
+                                       "perimeter_au_norm",
+                                       "min_dist_pxs",
+                                       "min_dist_micron",
+                                       "sqrt(area/pi)",
+                                       "min_dist/(sqrt(area/pi))",
+                                       "comment", "comment_long"])
+
+    for i in glob.glob(o_path + "*"):
+        filename = str(i).split('\\')[-1]
+        data = pd.read_csv(i)
+        data["date"] = filename[4:12]
+        data["file"] = filename[4:-4]
+
+        dots = data["particle"].unique()
+        for i in dots:
+            data_p = data[data["particle"] == i]
+            data_p["diff_y_micron"] = data_p["y_micron"].diff()
+            data_p["diff_x_micron"] = data_p["x_micron"].diff()
+            data_p["diff_xy_micron**2"] = ((data_p["diff_x_micron"]) ** 2 + (data_p["diff_y_micron"]) ** 2)
+            data_p["diff_xy_micron"] = data_p["diff_xy_micron**2"] ** .5
+            data_p["sqrt(area/pi)"] = (data_p["area_micron"] / math.pi) ** .5
+            data_p["min_dist/(sqrt(area/pi))"] = data_p["min_dist_micron"] / data_p["sqrt(area/pi)"]
+            aggregated = aggregated.append(data_p, ignore_index=True, sort=False)
+        print(filename + ' is ok')
+    aggregated.set_index(["file", "particle"], inplace=True)
+
+    aggregated.iloc[:, 0:26].to_csv(o_path[:-1] +
+                                    "_aggregated/aggregated_n_track.csv")
+"""
+
 # todo: test below to be removed, or make bash script from it? main method?
+
+
+inp_path = 'to_become_public/example_images/*ome.tif'
+out_path = 'to_become_public/tracking_output/'
+
+
 tstlst = glob.glob(inp_path, recursive=True)
 print(tstlst)
 filename = str(tstlst[1]).split('\\')[-1]
@@ -112,6 +163,7 @@ print(filename)
 (tiff, meta) = ome.read_ometiff(tstlst[1])
 xy_pxsize = float((meta.split(' ')[57]).split("\"")[1])
 # todo: parse xml as Harri explained, https://www.geeksforgeeks.org/xml-parsing-python/
+label_serum = int(filename.split('_')[1])
 
 chrom = rescale_intensity(tiff[:,0,0,:,:])
 lamin = rescale_intensity(tiff[:,0,1,:,:])
@@ -122,6 +174,5 @@ chrom_r, masks, edges = regis(chrom, lamin, fast = False)
 data = find_dots_TL(chrom_r)
 #tp.annotate(data.iloc[:4], chrom_r[0,:,:])
 
-add_data = analyse_data(data, masks, edges, xy_pxsize)
-add_data.to_csv(out_path + filename.split('.tif')[0] + ".csv")
-# serum_concentr from filename?
+add_data = analyse_data(data, masks, edges, xy_pxsize, label_serum)
+
