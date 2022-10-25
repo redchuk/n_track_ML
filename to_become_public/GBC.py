0680189ac4f59b348a63c115422ded3bd0fac078
Taras Redchuk <taras.redchuk@gmail.com>
2022-10-25 10:27:27 +0300

    pack feature engineering into a function, start GBC, comment

---
+++ to_become_public/GBC.py
@@ -0,0 +1,4 @@
+#todo: uncomment before publishing
+#from feature_engineering import get_data
+
+X, y = get_data('to_become_public/tracking_output/data_47091baa.csv')
--- to_become_public/feature_engineering.py
+++ to_become_public/feature_engineering.py
@@ -2,98 +2,100 @@ import pandas as pd
 import numpy as np
 from scipy.stats import linregress
 
-data = pd.read_csv('to_become_public/tracking_output/data_47091baa.csv')
 
-data['dX'] = data['x_micron'].diff()
-data['dY'] = data['y_micron'].diff()
+def get_data(path):
+    data = pd.read_csv(path)
 
-data_agg = data.groupby(['file', 'particle']).agg(serum=('serum', 'first'),
+    data['dX'] = data['x_micron'].diff()
+    data['dY'] = data['y_micron'].diff()
 
-                                                  MD=('D', 'mean'),
-                                                  # Mean locus displacement
-                                                  MaxD=('D', 'max'),
-                                                  # Maximal locus displacement
-                                                  temp_sum_D=('D', 'sum'),
-                                                  VarD=('D', 'var'),
-                                                  # Variance of locus displacement
+    data_agg = data.groupby(['file', 'particle']).agg(serum=('serum', 'first'),
 
-                                                  temp_sum_dX=('dX', 'sum'),
-                                                  temp_sum_dY=('dY', 'sum'),
+                                                      MD=('D', 'mean'),
+                                                      # Mean locus displacement
+                                                      MaxD=('D', 'max'),
+                                                      # Maximal locus displacement
+                                                      temp_sum_D=('D', 'sum'),
+                                                      VarD=('D', 'var'),
+                                                      # Variance of locus displacement
 
-                                                  MA=('A', 'mean'),
-                                                  # Mean nuclear area
-                                                  MP=('P', 'mean'),
-                                                  # Mean nuclear perimeter
+                                                      temp_sum_dX=('dX', 'sum'),
+                                                      temp_sum_dY=('dY', 'sum'),
 
-                                                  MDist=('Dist', 'mean'),
-                                                  # Mean locus distance to nuclear periphery
-                                                  temp_min_Dist=('Dist', 'min'),
-                                                  temp_max_Dist=('Dist', 'max'),
-                                                  temp_beg_Dist=('Dist', 'first'),
-                                                  temp_end_Dist=('Dist', 'last'),
-                                                  VarDist=('Dist', 'var'),
-                                                  # Variance of locus distance to nuclear periphery
-                                                  )
+                                                      MA=('A', 'mean'),
+                                                      # Mean nuclear area
+                                                      MP=('P', 'mean'),
+                                                      # Mean nuclear perimeter
 
-data_agg['rVarD'] = data_agg['VarD'] / data_agg['MD']
-# Relative variance of locus displacement
-data_agg['rVarDist'] = data_agg['VarDist'] / data_agg['MDist']
-# Relative variance of locus distance to nuclear periphery
+                                                      MDist=('Dist', 'mean'),
+                                                      # Mean locus distance to nuclear periphery
+                                                      temp_min_Dist=('Dist', 'min'),
+                                                      temp_max_Dist=('Dist', 'max'),
+                                                      temp_beg_Dist=('Dist', 'first'),
+                                                      temp_end_Dist=('Dist', 'last'),
+                                                      VarDist=('Dist', 'var'),
+                                                      # Variance of locus distance to nuclear periphery
+                                                      )
 
-data_agg['TD'] = np.sqrt((data_agg['temp_sum_dX']) ** 2 + (data_agg['temp_sum_dY']) ** 2)
-# Total locus displacement
-data_agg['Pers'] = data_agg['TD'] / data_agg['temp_sum_D']
-# Persistence of locus movement
+    data_agg['rVarD'] = data_agg['VarD'] / data_agg['MD']
+    # Relative variance of locus displacement
+    data_agg['rVarDist'] = data_agg['VarDist'] / data_agg['MDist']
+    # Relative variance of locus distance to nuclear periphery
 
-data_agg['temp_file_MD'] = data_agg.groupby('file')['MD'].transform(np.max)
-data_agg['ifFast'] = np.where((data_agg['MD'] == data_agg['temp_file_MD']), 1, 0)
-# True for the fastest homologous locus in the nucleus (highest MD)
+    data_agg['TD'] = np.sqrt((data_agg['temp_sum_dX']) ** 2 + (data_agg['temp_sum_dY']) ** 2)
+    # Total locus displacement
+    data_agg['Pers'] = data_agg['TD'] / data_agg['temp_sum_D']
+    # Persistence of locus movement
 
-data_agg['DistR'] = data_agg['temp_max_Dist'] - data_agg['temp_min_Dist']
-# Range of locus distance to nuclear periphery
-data_agg['TDist'] = data_agg['temp_end_Dist'] - data_agg['temp_beg_Dist']
-# Total locus radial displacement
+    data_agg['temp_file_MD'] = data_agg.groupby('file')['MD'].transform(np.max)
+    data_agg['ifFast'] = np.where((data_agg['MD'] == data_agg['temp_file_MD']), 1, 0)
+    # True for the fastest homologous locus in the nucleus (highest MD)
 
-data_agg['temp_file_max_Dist'] = data_agg.groupby('file')['MDist'].transform(np.max)
-data_agg['ifCentr'] = np.where((data_agg['MDist'] == data_agg['temp_file_max_Dist']), 1, 0)
-# True for the homologous locus furthest from nuclear rim (highest MDist)
+    data_agg['DistR'] = data_agg['temp_max_Dist'] - data_agg['temp_min_Dist']
+    # Range of locus distance to nuclear periphery
+    data_agg['TDist'] = data_agg['temp_end_Dist'] - data_agg['temp_beg_Dist']
+    # Total locus radial displacement
 
-data_slope = data.groupby(['file', 'particle']).apply(lambda x: linregress(x['frame'], x['Dist'])[0])
-data_agg['sDist'] = data_slope
-# Trend of change of locus distance to nuclear periphery
+    data_agg['temp_file_max_Dist'] = data_agg.groupby('file')['MDist'].transform(np.max)
+    data_agg['ifCentr'] = np.where((data_agg['MDist'] == data_agg['temp_file_max_Dist']), 1, 0)
+    # True for the homologous locus furthest from nuclear rim (highest MDist)
 
-data_sA = data.groupby(['file', 'particle']).apply(lambda x: linregress(x['frame'], x['A'])[0])
-data_agg['sA'] = data_sA
-# Trend of change of nuclear area
+    data_slope = data.groupby(['file', 'particle']).apply(lambda x: linregress(x['frame'], x['Dist'])[0])
+    data_agg['sDist'] = data_slope
+    # Trend of change of locus distance to nuclear periphery
 
-data_sP = data.groupby(['file', 'particle']).apply(lambda x: linregress(x['frame'], x['P'])[0])
-data_agg['sP'] = data_sP
-# Trend of change of nuclear perimeter
+    data_sA = data.groupby(['file', 'particle']).apply(lambda x: linregress(x['frame'], x['A'])[0])
+    data_agg['sA'] = data_sA
+    # Trend of change of nuclear area
 
-data_SD_D = data.groupby(['file', 'particle']).agg(SD_diff=('D', 'std'))
-data_i = data.set_index(['file', 'particle'])
-data_i['SD_D'] = data_SD_D
-data_i['MD'] = data_agg['MD']
-data_i['out2sd'] = np.where((data_i['D'] > (data_i['MD'] + 2 * data_i['SD_D'])), 1, 0)
-data_i['out3sd'] = np.where((data_i['D'] > (data_i['MD'] + 3 * data_i['SD_D'])), 1, 0)
-data_agg['out2sd'] = data_i.groupby(['file', 'particle']).agg(out2sd=('out2sd', 'sum'))
-# Number of displacements 2SD longer than MD
-data_agg['out3sd'] = data_i.groupby(['file', 'particle']).agg(out3sd=('out3sd', 'sum'))
-# Number of displacements 3SD longer than MD
+    data_sP = data.groupby(['file', 'particle']).apply(lambda x: linregress(x['frame'], x['P'])[0])
+    data_agg['sP'] = data_sP
+    # Trend of change of nuclear perimeter
 
-data = data_agg.drop(['temp_sum_dX',
-                      'temp_sum_dY',
-                      'temp_min_Dist',
-                      'temp_max_Dist',
-                      'temp_beg_Dist',
-                      'temp_end_Dist',
-                      'temp_file_MD',
-                      'temp_file_max_Dist',
-                      'temp_sum_D',
-                      ], axis=1)
-data.reset_index(inplace=True)
+    data_SD_D = data.groupby(['file', 'particle']).agg(SD_diff=('D', 'std'))
+    data_i = data.set_index(['file', 'particle'])
+    data_i['SD_D'] = data_SD_D
+    data_i['MD'] = data_agg['MD']
+    data_i['out2sd'] = np.where((data_i['D'] > (data_i['MD'] + 2 * data_i['SD_D'])), 1, 0)
+    data_i['out3sd'] = np.where((data_i['D'] > (data_i['MD'] + 3 * data_i['SD_D'])), 1, 0)
+    data_agg['out2sd'] = data_i.groupby(['file', 'particle']).agg(out2sd=('out2sd', 'sum'))
+    # Number of displacements 2SD longer than MD
+    data_agg['out3sd'] = data_i.groupby(['file', 'particle']).agg(out3sd=('out3sd', 'sum'))
+    # Number of displacements 3SD longer than MD
 
-features = ['MD', 'MaxD', 'VarD', 'MA', 'MP', 'MDist', 'VarDist', 'rVarD', 'rVarDist',
-            'TD', 'Pers', 'ifFast', 'DistR', 'TDist', 'ifCentr', 'sDist', 'sA', 'sP', 'out2sd', 'out3sd']
+    data = data_agg.drop(['temp_sum_dX',
+                          'temp_sum_dY',
+                          'temp_min_Dist',
+                          'temp_max_Dist',
+                          'temp_beg_Dist',
+                          'temp_end_Dist',
+                          'temp_file_MD',
+                          'temp_file_max_Dist',
+                          'temp_sum_D',
+                          ], axis=1)
+    data.reset_index(inplace=True)
 
-# todo: pack into a function to use in gbc
+    features = ['MD', 'MaxD', 'VarD', 'MA', 'MP', 'MDist', 'VarDist', 'rVarD', 'rVarDist',
+                'TD', 'Pers', 'ifFast', 'DistR', 'TDist', 'ifCentr', 'sDist', 'sA', 'sP', 'out2sd', 'out3sd']
+
+    return data[features], data['serum']
