81907bf981cd380a816aec830e22312db697e259
Lassi Paavolainen <lassi.paavolainen@gmail.com>
2023-02-07 11:40:53 +0200

    Minor tweaks to tracking.py code

--- to_become_public/tracking.py
+++ to_become_public/tracking.py
@@ -1,4 +1,3 @@
-import glob
 import numpy as np
 import trackpy as tp
 from skimage import img_as_float
@@ -10,6 +9,7 @@ from pystackreg import StackReg
 import pandas as pd
 import subprocess
 import xml.etree.ElementTree as ET
+from pathlib import Path
 
 
 def get_git_hash():
@@ -41,13 +41,10 @@ def edge(binary):
 
 
 def regis(chrom, lamin, fast=False):
+    ch_mask = np.zeros_like(chrom, dtype=np.bool_)
     for t in range(chrom.shape[0]):
-        ch = chrom[t]
-        ch[np.invert(binary(lamin[t]))] = 0
-
-    ch_mask = np.copy(chrom)
-    for i in range(chrom.shape[0]):
-        ch_mask[i] = (ch_mask[i] > 0)
+        ch_mask[t] = binary(lamin[t])
+        chrom[t][~ch_mask[t]] = 0
 
     if fast == False:
         sr = StackReg(StackReg.RIGID_BODY)
@@ -74,8 +71,8 @@ def find_dots(stack, minmass=100000, diameter=11, search_range=20, memory=3, thr
 
 
 def min_dist(df, edges_stack):
-    coord_x = np.asarray([i for i in range(edges_stack[0].shape[1])])
-    coord_y = np.asarray([i for i in range(edges_stack[0].shape[0])])
+    coord_x = np.asarray(list(range(edges_stack[0].shape[1])))
+    coord_y = np.asarray(list(range(edges_stack[0].shape[0])))
 
     xarr = np.zeros(edges_stack[0].shape)
     xarr[:, :] = coord_x
@@ -87,7 +84,7 @@ def min_dist(df, edges_stack):
     return df
 
 
-def analyse_data(dots_df, nucl_masks, nucl_edges, pxsize, file):
+def analyse_data(dots_df, nucl_masks, nucl_edges, pxsize, fname):
     dots_df['y_micron'] = dots_df['y'] * pxsize
     dots_df['x_micron'] = dots_df['x'] * pxsize
 
@@ -101,11 +98,12 @@ def analyse_data(dots_df, nucl_masks, nucl_edges, pxsize, file):
     data_dist['min_dist_micron'] = data_dist['min_dist_pxs'] * pxsize
 
     data_dist['script_version_git'] = get_git_hash()
-    data_dist['serum'] = int(file.split('_')[1])
-    data_dist['date'] = file.split('_')[0]
-    data_dist['file'] = file[:-4]
+    data_dist['serum'] = int(fname.split('_')[1])
+    data_dist['date'] = fname.split('_')[0]
+    data_dist['file'] = fname[:-4]
 
     return data_dist
+
 
 
 def add_xy_deltas(df):
@@ -130,61 +128,66 @@ def add_xy_deltas(df):
     dots = df['particle'].unique()
 
     for i in dots:
-        data_p = df[df['particle'] == i]
+        data_p = df[df['particle'] == i].copy()
         data_p['diff_y_micron'] = data_p['y_micron'].diff()
         data_p['diff_x_micron'] = data_p['x_micron'].diff()
         data_p['diff_xy_micron'] = ((data_p['diff_x_micron']) ** 2 + (data_p['diff_y_micron']) ** 2) ** .5
 
         data_a = data_a.append(data_p, ignore_index=True, sort=False)
 
-    data_a = data_a.iloc[:, 0:len(c_list)]
+    data_a = data_a[c_list]
 
     return data_a
 
 
-# todo: change paths before publication
-inp_path = 'to_become_public/example_images/*ome.tif'
-out_path = 'to_become_public/tracking_output/'
+def main():
+    # todo: change paths before publication
+    inp_path = Path('example_images')
+    out_path = Path('tracking_output')
 
-columns_raw = ['file',
-               'script_version_git',
-               'date',
-               'serum',
-               'particle',
-               'frame',
-               'y_micron',
-               'x_micron',
-               'diff_xy_micron',
-               'area_micron',
-               'perimeter_au_norm',
-               'min_dist_micron',
-               ]
+    columns_raw = ['file',
+                   'script_version_git',
+                   'date',
+                   'serum',
+                   'particle',
+                   'frame',
+                   'y_micron',
+                   'x_micron',
+                   'diff_xy_micron',
+                   'area_micron',
+                   'perimeter_au_norm',
+                   'min_dist_micron',
+    ]
 
-canonical_fnames = {'diff_xy_micron': 'D',      # Locus displacement, microns
-                    'area_micron': 'A',         # Nuclear area, square microns
-                    'perimeter_au_norm': 'P',   # Nuclear perimeter, arbitrary units
-                    'min_dist_micron': 'Dist',  # Minimal distance from locus to nuclear rim, microns
-                    }
+    canonical_fnames = {'diff_xy_micron': 'D',      # Locus displacement, microns
+                        'area_micron': 'A',         # Nuclear area, square microns
+                        'perimeter_au_norm': 'P',   # Nuclear perimeter, arbitrary units
+                        'min_dist_micron': 'Dist',  # Minimal distance from locus to nuclear rim, microns
+    }
 
-data_raw = pd.DataFrame(columns=columns_raw)
+    data_raw = pd.DataFrame(columns=columns_raw)
+    
+    for fpath in inp_path.rglob('*ome.tif'):
+        filename = fpath.name
 
-for file in glob.glob(inp_path, recursive=True):
-    filename = str(file).split('\\')[-1]
+        (tiff, meta) = ome.read_ometiff(fpath)
+        xy_pxsize = get_pxsize(meta)
+        label_serum = int(filename.split('_')[1])
 
-    (tiff, meta) = ome.read_ometiff(file)
-    xy_pxsize = get_pxsize(meta)
-    label_serum = int(filename.split('_')[1])
+        chrom = rescale_intensity(tiff[:, 0, 0, :, :])
+        lamin = rescale_intensity(tiff[:, 0, 1, :, :])
 
-    chrom = rescale_intensity(tiff[:, 0, 0, :, :])
-    lamin = rescale_intensity(tiff[:, 0, 1, :, :])
+        chrom_r, masks, edges = regis(chrom, lamin, fast=False)
+        data = find_dots(chrom_r)
 
-    chrom_r, masks, edges = regis(chrom, lamin, fast=False)
-    data = find_dots(chrom_r)
+        add_data = analyse_data(data, masks, edges, xy_pxsize, filename)
+        add_data = add_xy_deltas(add_data)
+        data_raw = data_raw.append(add_data, ignore_index=True, sort=False)
 
-    add_data = analyse_data(data, masks, edges, xy_pxsize, filename)
-    add_data = add_xy_deltas(add_data)
-    data_raw = data_raw.append(add_data, ignore_index=True, sort=False)
+        data_raw[columns_raw] \
+                .rename(columns=canonical_fnames) \
+                .to_csv(out_path / 'tracking_raw.csv')
 
-data_raw.iloc[:, 0:len(columns_raw)] \
-    .rename(columns=canonical_fnames) \
-    .to_csv(out_path + 'tracking_raw.csv')
+
+if __name__=="__main__":
+    main()
